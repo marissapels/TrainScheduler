13a9e748bc54032d0abaf0d3cbeba617d7a895df
Marissa Pels <marissapels@gmail.com>
2017-05-22 18:50:20 -0500

    upload 5/22/17

--- train.js
+++ train.js
@@ -9,7 +9,7 @@
   };
   firebase.initializeApp(config);
 
-  var database=firebase.database();
+ var database=firebase.database();
 
   var name="";
   var destination="";
@@ -18,27 +18,28 @@
   var nextArrival="";
   var minutesAway="";
 
+
   $("#submit").on("click", function(){
-  	event.preventDefault();
+    event.preventDefault();
 
-  	name=$("#name-input").val().trim();
-  	destination=$("#destination-input").val().trim();
-  	var firstTimeInput=$("#time-input").val().trim();
-    firstTime=moment(firstTimeInput, "HH:mm").format("HH:mm");
+    name=$("#name-input").val().trim();
+    destination=$("#destination-input").val().trim();
+    var firstTimeInput=$("#time-input").val().trim();
+    firstTime=moment(firstTimeInput, "HH:mm").format();
     console.log(firstTime);
-  	frequency=$("#frequency-input").val().trim();
+    frequency=$("#frequency-input").val().trim();
 
-  	database.ref().push({
-  		name: name,
-  		destination: destination,
-  		firstTime: firstTime,
-  		frequency: frequency,
-  	});
+    database.ref().push({
+      name: name,
+      destination: destination,
+      firstTime: firstTime,
+      frequency: frequency,
+    });
 
-  	$("#name-input").val("");
-  	$("#destination-input").val("");
-  	$("#time-input").val("");
-  	$("#frequency-input").val("");
+    $("#name-input").val("");
+    $("#destination-input").val("");
+    $("#time-input").val("");
+    $("#frequency-input").val("");
 
   });
 
@@ -47,23 +48,38 @@ database.ref().on("child_added", function(snapshot){
   var destination=(snapshot.val().destination);
   var frequency=(snapshot.val().frequency);
   var firstTime=(snapshot.val().firstTime);
-  minutesAway=frequency-((moment().diff(moment(firstTime, "HH:mm"),"minutes"))%frequency);
-  nextArrival=moment(moment().add(minutesAway, "minutes")).format("hh:mm a");
+  if (moment(firstTime).unix()<moment().unix()){
+    minutesAway=frequency-((moment().diff(moment(firstTime),"minutes"))%frequency);
+    nextArrival=moment(moment().add(minutesAway, "minutes")).format("hh:mm a");
+  }
+  else {
+    nextArrival=moment(firstTime).format("hh:mm a");
+    minutesAway=moment(firstTime).diff(moment(),"minutes");
+  }
 
+  console.log(database.ref().push().key);
 
-	var newTrain=
-		"<tr>" +
-      // "<td><button class='remove'>" + "X" + "</button></td>"+
-			"<td>" + name + "</td>" +
-			"<td>" + destination + "</td>" +
-			"<td>" + frequency + "</td>" +
-			"<td>" + nextArrival +
-			"<td>" + minutesAway + "</td>" +
-		"</tr>";
-
-
-	$(".table").append(newTrain);
-
+  var newTrain=
+    "<tr class='"+ snapshot.key + "'>" +
+      "<td><button class='remove' id='"+ snapshot.key + "'>" + "X" + "</button></td>"+
+      "<td>" + name + "</td>" +
+      "<td>" + destination + "</td>" +
+      "<td>" + frequency + "</td>" +
+      "<td>" + nextArrival +
+      "<td>" + minutesAway + "</td>" +
+    "</tr>";
+  $(".table").append(newTrain);
   }, function(errorObject) {
   console.log("The read failed: " + errorObject.code);
+});
+
+$(document).on("click", ".remove",function(){
+  event.preventDefault();
+  var dataKey=$(this).attr("id");
+  database.ref().child(dataKey).remove();
+
+});
+
+database.ref().on("child_removed", function(snapshot){
+  $("."+snapshot.key).remove();
 });
